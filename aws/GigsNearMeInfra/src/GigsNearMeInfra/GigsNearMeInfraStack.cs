0c88e73c9698d1831a35c01b158ebb0153d89b7f
PHIDELIST <phidelisoluoch@gmail.com>
2024-02-03 10:27:24 +0300

    Added EC2 and autoscaling group

--- aws/GigsNearMeInfra/src/GigsNearMeInfra/GigsNearMeInfraStack.cs
+++ aws/GigsNearMeInfra/src/GigsNearMeInfra/GigsNearMeInfraStack.cs
@@ -29,6 +29,48 @@ namespace GigsNearMeInfra
                         }
                     }
             });
+            var scalingGroup = new AutoScalingGroup(this, "ASG", new AutoScalingGroupProps
+            {
+                Vpc = vpc,
+                VpcSubnets = new SubnetSelection
+                {
+                    SubnetType = SubnetType.PRIVATE_ISOLATED
+                },
+                MachineImage = MachineImage.LatestAmazonLinux(new AmazonLinuxImageProps
+                {
+                    Generation = AmazonLinuxGeneration.AMAZON_LINUX_2
+                }),
+                InstanceType = InstanceType.Of(InstanceClass.BURSTABLE3, InstanceSize.MICRO),
+                MinCapacity = 1,
+                MaxCapacity = 2,
+                AllowAllOutbound = true,
+                Role = appRole,
+                Signals = Signals.WaitForCount(1, new SignalsOptions
+                {
+                    Timeout = Duration.Minutes(10)
+                }),
+            });
+            scalingGroup.AddUserData(new string[]
+            {
+                "yum -y update",
+                "rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm",
+                "yum -y install dotnet-runtime-5.0",
+                "yum -y install aspnetcore-runtime-5.0",
+                "yum -y install ruby",
+                "yum -y install wget",
+                "cd /tmp",
+                $"wget https://aws-codedeploy-{props.Env.Region}.s3.{props.Env.Region}.amazonaws.com/latest/install",
+                "chmod +x ./install",
+                "./install auto",
+                "service codedeploy-agent start",
+            });
+
+            scalingGroup.AddUserData(new string[]
+            {
+                "curl https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/Curation/DotNet/CDK/deploySampleApp.sh | bash"
+            });
+
+            scalingGroup.UserData.AddSignalOnExitCommand(scalingGroup);
         }
     }
 }
