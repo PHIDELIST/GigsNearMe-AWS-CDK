a2c73f5327889905197ed0b9230a4d119a5a00fd
PHIDELIST <64526896+PHIDELIST@users.noreply.github.com>
2024-02-09 10:13:22 +0300

    DevOps (#7)

--- aws/GigsNearMeInfra/src/GigsNearMeInfra/GigsNearMeInfraStack.cs
+++ aws/GigsNearMeInfra/src/GigsNearMeInfra/GigsNearMeInfraStack.cs
@@ -1,4 +1,6 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.S3;
 using Amazon.CDK.AWS.
 using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.SSM
@@ -31,6 +33,102 @@ namespace GigsNearMeInfra
                             Name = "GigsNearMePrivate"
                         }
                     }
+            });
+            //S3 bucket to store artifacts
+            var artifactsBucket = new Bucket(this, "ArtifactsBucket", new BucketProps
+            {
+                Versioned = true,
+                RemovalPolicy = RemovalPolicy.DESTROY,
+                AutoDeleteObjects = true
+            });
+
+            new CfnOutput(this, "ArtifactsBucketName", new CfnOutputProps
+            {
+                Value = artifactsBucket.BucketName
+            });
+            //Cloud Trail 
+            var trail = new Trail(this, "GigsNearMeTrail", new TrailProps
+            {
+                Bucket = new Bucket(this, "TrailLogsBucket", new BucketProps
+                {
+                    RemovalPolicy = RemovalPolicy.DESTROY,
+                    AutoDeleteObjects = true
+                })
+            });
+
+            trail.AddS3EventSelector(
+                new[]
+                {
+            new S3EventSelector
+            {
+                Bucket = artifactsBucket
+            }
+                    },
+                    new AddEventSelectorOptions
+                    {
+                        ReadWriteType = ReadWriteType.WRITE_ONLY
+                    }
+                );
+            
+            //AWS CODE PIPLELINE TO DEPLOY THE APP
+            var codeDeployApp = new ServerApplication(this, "GigsNearMe", new ServerApplicationProps
+                {
+                    ApplicationName = "GigsNearMe"
+                });
+            var deploymentGroup = new ServerDeploymentGroup(this, "WebHostDG", new ServerDeploymentGroupProps
+            {
+                Application = codeDeployApp,
+                AutoScalingGroups = new AutoScalingGroup[] { scalingGroup },
+                DeploymentGroupName = "DeploymentGroup",
+                InstallAgent = false, // we did this already as part of EC2 instance intitialization userdata
+                Role = new Role(this, "DeploymentRole", new RoleProps
+                {
+                    AssumedBy = new ServicePrincipal("codedeploy.amazonaws.com"),
+                    ManagedPolicies = new IManagedPolicy[]
+                    {
+                        ManagedPolicy.FromAwsManagedPolicyName("service-role/AWSCodeDeployRole")
+                    }
+                }),
+                DeploymentConfig = ServerDeploymentConfig.ONE_AT_A_TIME
+            });
+
+            const string DeploymentArtifactName = "GigsNearMeDeploymentBundle.zip";
+
+            new CfnOutput(this, "DeploymentArtifactName", new CfnOutputProps {
+                Value = DeploymentArtifactName
+            });
+
+            var deploymentArtifact = new Artifact_("DeploymentArtifact");
+
+            var pipeline = new Pipeline(this, "CiCdPipeline", new PipelineProps
+            {
+                ArtifactBucket = artifactsBucket,
+                Stages = new [] {
+                    new Amazon.CDK.AWS.CodePipeline.StageProps {
+                        StageName = "Download",
+                        Actions = new [] {
+                            new S3SourceAction(new S3SourceActionProps {
+                                ActionName = "DownloadBundle",
+                                RunOrder = 1,
+                                Bucket = artifactsBucket,
+                                BucketKey = DeploymentArtifactName,
+                                Trigger = S3Trigger.EVENTS,
+                                Output = deploymentArtifact
+                            })
+                        }
+                    },
+                    new Amazon.CDK.AWS.CodePipeline.StageProps {
+                        StageName = "Deploy",
+                        Actions = new [] {
+                            new CodeDeployServerDeployAction(new CodeDeployServerDeployActionProps {
+                                ActionName = "DeployViaCodeDeploy",
+                                RunOrder = 2,
+                                DeploymentGroup = deploymentGroup,
+                                Input = deploymentArtifact
+                            })
+                        }
+                    }
+                }
             });
 
             var loadBalancer = new ApplicationLoadBalancer(this, "LB", new ApplicationLoadBalancerProps
@@ -153,4 +251,5 @@ namespace GigsNearMeInfra
             });
         }
     }
+}
 }
